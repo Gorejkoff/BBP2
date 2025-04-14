0abd59b5b026055bd1e6ccb82e8815f36869466c
gorejkoff <neskazua63@gmail.com>
2025-04-14 23:46:36 +0300

    first commit

---
+++ dist/js/app.js
@@ -0,0 +1,203 @@
+"use strict"
+
+// window.addEventListener('load', (event) => {});
+
+// desktop or mobile (mouse or touchscreen)
+const isMobile = {
+   Android: function () { return navigator.userAgent.match(/Android/i) },
+   BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i) },
+   iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i) },
+   Opera: function () { return navigator.userAgent.match(/Opera Mini/i) },
+   Windows: function () { return navigator.userAgent.match(/IEMobile/i) },
+   any: function () {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+   }
+};
+const isPC = !isMobile.any();
+if (isPC) { document.body.classList.add('_pc') } else { document.body.classList.add('_touch') };
+
+// media queries
+const MIN1024 = window.matchMedia('(min-width: 1024px)');
+const MIN780 = window.matchMedia('(min-width: 780px)');
+
+// variables
+const HEADER = document.getElementById('header');
+
+function throttle(callee, timeout) {
+   let timer = null;
+   return function perform(...args) {
+      if (timer) return;
+      timer = setTimeout(() => {
+         callee(...args);
+         clearTimeout(timer);
+         timer = null;
+      }, timeout)
+   }
+}
+
+/* запись переменных высоты элементов */
+function addHeightVariable() {
+   if (typeof HEADER !== "undefined") {
+      document.body.style.setProperty('--height-header', `${HEADER.offsetHeight}px`)
+   }
+}
+addHeightVariable();
+
+// ** ======================= RESIZE ======================  ** //
+window.addEventListener('resize', () => {
+   addHeightVariable();
+   closeHeaderMenu();
+})
+
+
+// ** ======================= CLICK ======================  ** //
+document.documentElement.addEventListener("click", (event) => {
+   if (event.target.closest('.open-menu')) { openHeaderMenu() }
+})
+
+function openHeaderMenu() {
+   document.body.classList.toggle('menu-is-open')
+}
+function closeHeaderMenu() {
+   document.body.classList.remove('menu-is-open')
+}
+
+
+if (MIN780.matches) {
+   const FIRST_SCREEN_ANIMATE = document.getElementById('first-screen-animate');
+   function startAnimateFirstScreen() {
+      if (FIRST_SCREEN_ANIMATE) FIRST_SCREEN_ANIMATE.classList.add('show-background');
+   }
+   setTimeout(startAnimateFirstScreen, 500);
+}
+
+window.addEventListener('load', function (event) {
+   gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin);
+   let smoother = ScrollSmoother.create({
+      smooth: 1,
+      effects: true
+   });
+
+   document.body.addEventListener('click', (event) => {
+      if (event.target.closest('[href^="#"]')) {
+         event.preventDefault();
+         let getName = event.target.closest('[href^="#"]').getAttribute('href');
+         closeHeaderMenu();
+         gsap.to(window, { scrollTo: getName, ease: "power2" })
+      }
+   })
+
+})
+
+
+// перемещение блоков при адаптиве
+// data-da=".class,3,768" 
+// класс родителя куда перемещать
+// порядковый номер в родительском блоке куда перемещается начиная с 0 как индексы массива
+// ширина экрана min-width
+// два перемещения: data-da=".class,3,768,.class2,1,1024"
+const ARRAY_DATA_DA = document.querySelectorAll('[data-da]');
+ARRAY_DATA_DA.forEach(function (e) {
+   const dataArray = e.dataset.da.split(',');
+   const addressMove = searchDestination(e, dataArray[0]);
+   const addressMoveSecond = dataArray[3] && searchDestination(e, dataArray[3]);
+   const addressParent = e.parentElement;
+   const listChildren = addressParent.children;
+   const mediaQuery = window.matchMedia(`(min-width: ${dataArray[2]}px)`);
+   const mediaQuerySecond = dataArray[5] && window.matchMedia(`(min-width: ${dataArray[5]}px)`);
+   for (let i = 0; i < listChildren.length; i++) { !listChildren[i].dataset.n && listChildren[i].setAttribute('data-n', `${i}`) };
+   mediaQuery.matches && startChange(mediaQuery, addressMove, e, listChildren, addressParent, dataArray);
+   if (mediaQuerySecond && mediaQuerySecond.matches) moving(e, dataArray[4], addressMoveSecond);
+   mediaQuery.addEventListener('change', () => { startChange(mediaQuery, addressMove, e, listChildren, addressParent, dataArray) });
+   if (mediaQuerySecond) mediaQuerySecond.addEventListener('change', () => {
+      if (mediaQuerySecond.matches) { moving(e, dataArray[4], addressMoveSecond); return; };
+      startChange(mediaQuery, addressMove, e, listChildren, addressParent, dataArray);
+   });
+});
+
+function startChange(mediaQuery, addressMove, e, listChildren, addressParent, dataArray) {
+   if (mediaQuery.matches) { moving(e, dataArray[1], addressMove); return; }
+   if (listChildren.length > 0) {
+      for (let z = 0; z < listChildren.length; z++) {
+         if (listChildren[z].dataset.n > e.dataset.n) {
+            listChildren[z].before(e);
+            break;
+         } else if (z == listChildren.length - 1) {
+            addressParent.append(e);
+         }
+      }
+      return;
+   }
+   addressParent.prepend(e);
+};
+
+function searchDestination(e, n) {
+   if (e.classList.contains(n.slice(1))) { return e }
+   if (e.parentElement.querySelector(n)) { return e.parentElement.querySelector(n) };
+   return searchDestination(e.parentElement, n);
+}
+
+function moving(e, order, addressMove) {
+   if (order == "first") { addressMove.prepend(e); return; };
+   if (order == "last") { addressMove.append(e); return; };
+   if (addressMove.children[order]) { addressMove.children[order].before(e); return; }
+   addressMove.append(e);
+}
+
+
+
+/* открывает, закрывает модальные окна. */
+/*
+добавить классы
+js-modal-hidden - родительский контейнер модального окна который скрывается и показывается, задать стили скрытия
+js-modal-visible - задать стили открытия
+js-modal-close - кнопка закрытия модального окна находится внутри js-modal-hidde
+кнопка открытия, любая:
+js-modal-open - кнопка открытия модального окна
+data-modal_open="id" - id модального окна
+если надо что бы окно закрывалось при клике на пустое место (фон), добавляется атрибут js-modal-stop-close.
+js-modal-stop-close - атрибут указывает на поле, при клике на которое не должно происходить закрытие окна, 
+т.е. контейнер контента, при этом внешний родительский контейнет помечается атрибутом js-modal-close.
+допускается дополнительно кнопка закрытия внутри js-modal-stop-close.
+*/
+document.addEventListener('click', (event) => {
+   if (event.target.closest('.js-modal-open')) { openModal(event) }
+   if (event.target.closest('.js-modal-close')) { testModalStopClose(event) }
+})
+function openModal(event) {
+   let modalElement = event.target.closest('.js-modal-open').dataset.modal_open;
+   if (typeof modalElement !== "undefined" && document.querySelector(`#${modalElement}`)) {
+      document.querySelector(`#${modalElement}`).classList.add('js-modal-visible');
+      document.body.classList.add('body-overflow')
+   }
+}
+function testModalStopClose(event) {
+   if (event.target.closest('.js-modal-stop-close') &&
+      event.target.closest('.js-modal-stop-close') !==
+      event.target.closest('.js-modal-close').closest('.js-modal-stop-close')) {
+      return
+   }
+   closeModal(event);
+}
+function closeModal(event) {
+   event.target.closest('.js-modal-hidden').classList.remove('js-modal-visible');
+   if (!document.querySelector('.js-modal-visible')) {
+      document.body.classList.remove('body-overflow');
+   }
+}
+// функция закрытия модального окна (передать id модального окна)
+function initCloseModal(modalElement) {
+   if (document.querySelector(`#${modalElement}`)) {
+      document.querySelector(`#${modalElement}`).classList.remove('js-modal-visible');
+   }
+   if (!document.querySelector('.js-modal-visible')) {
+      document.body.classList.remove('body-overflow');
+   }
+}
+// функция открытия модального окна (передать id модального окна)
+function initOpenModal(modalElement) {
+   if (document.querySelector(`#${modalElement}`)) {
+      document.querySelector(`#${modalElement}`).classList.add('js-modal-visible');
+      document.body.classList.add('body-overflow')
+   }
+}
